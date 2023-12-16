a29315b191fce536fbc8143c2fbd9b2920173e3a
sinisa-andric <sinisa.andric@mail.ru>
2023-12-16 13:15:47 +0100

    feat: small changes in service.go

--- service/service.go
+++ service/service.go
@@ -3,6 +3,10 @@ package service
 import (
 	"errors"
 	"fmt"
+	"log"
+	"net"
+	"time"
+
 	"github.com/c12s/meridian/helper"
 	"github.com/c12s/meridian/model"
 	"github.com/c12s/meridian/storage"
@@ -11,16 +15,13 @@ import (
 	sg "github.com/c12s/stellar-go"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
-	"log"
-	"net"
-	"time"
 )
 
 type Server struct {
 	instrument map[string]string
 	db         storage.DB
 	apollo     string
-	meridian   string
+	//meridian   string
 }
 
 func (s *Server) List(ctx context.Context, req *cPb.ListReq) (*cPb.ListResp, error) {
@@ -30,19 +31,19 @@ func (s *Server) List(ctx context.Context, req *cPb.ListReq) (*cPb.ListResp, err
 
 	token, err := helper.ExtractToken(ctx)
 	if err != nil {
-		span.AddLog(&sg.KV{"token error", err.Error()})
+		span.AddLog(&sg.KV{Key: "token error", Value: err.Error()})
 		return nil, err
 	}
 
 	err = s.auth(ctx, listOpt(req, token))
 	if err != nil {
-		span.AddLog(&sg.KV{"auth error", err.Error()})
+		span.AddLog(&sg.KV{Key: "auth error", Value: err.Error()})
 		return nil, err
 	}
 
 	_, err = s.checkNS(ctx, req.Extras["user"], req.Extras["namespace"])
 	if err != nil {
-		span.AddLog(&sg.KV{"check ns error", err.Error()})
+		span.AddLog(&sg.KV{Key: "check ns error", Value: err.Error()})
 		return nil, err
 	}
 
@@ -54,7 +55,7 @@ func (s *Server) List(ctx context.Context, req *cPb.ListReq) (*cPb.ListResp, err
 		req.Extras,
 	)
 	if err != nil {
-		span.AddLog(&sg.KV{"roles list error", err.Error()})
+		span.AddLog(&sg.KV{Key: "roles list error", Value: err.Error()})
 		return nil, err
 	}
 	return rsp, nil
@@ -67,21 +68,25 @@ func (s *Server) Mutate(ctx context.Context, req *cPb.MutateReq) (*cPb.MutateRes
 
 	token, err := helper.ExtractToken(ctx)
 	if err != nil {
-		span.AddLog(&sg.KV{"token error", err.Error()})
+		span.AddLog(&sg.KV{Key: "token error", Value: err.Error()})
 		return nil, err
 	}
 	fmt.Println("{{TOKEN MUTATE MERIDIAN}}", token)
 
 	err = s.auth(ctx, mutateOpt(req, token))
 	if err != nil {
-		span.AddLog(&sg.KV{"auth error", err.Error()})
+		span.AddLog(&sg.KV{Key: "auth error", Value: err.Error()})
 		return nil, err
 	}
 
 	ns, err := s.checkNS(ctx, req.Mutate.UserId, req.Mutate.Extras["namespace"])
+	if err != nil {
+		span.AddLog(&sg.KV{Key: "namespace check error", Value: err.Error()})
+		return nil, err
+	}
 	if ns != "" {
-		span.AddLog(&sg.KV{"check ns error", "Namespace already exis"})
-		return nil, errors.New("Namespace already exis")
+		span.AddLog(&sg.KV{Key: "check ns error", Value: "Namespace already exis"})
+		return nil, errors.New("namespace already exis")
 	}
 
 	err, rsp := s.db.Mutate(helper.AppendToken(
@@ -89,7 +94,7 @@ func (s *Server) Mutate(ctx context.Context, req *cPb.MutateReq) (*cPb.MutateRes
 		token,
 	), req)
 	if err != nil {
-		span.AddLog(&sg.KV{"namespace mutate error", err.Error()})
+		span.AddLog(&sg.KV{Key: "namespace mutate error", Value: err.Error()})
 		return nil, err
 	}
 	return rsp, nil
@@ -105,7 +110,7 @@ func (s *Server) Exists(ctx context.Context, req *mPb.NSReq) (*mPb.NSResp, error
 	)
 	if err != nil {
 		fmt.Println("{{service.Exists err != nil}}")
-		span.AddLog(&sg.KV{"namespace exists error", err.Error()})
+		span.AddLog(&sg.KV{Key: "namespace exists error", Value: err.Error()})
 		return nil, err
 	}
 
@@ -113,21 +118,22 @@ func (s *Server) Exists(ctx context.Context, req *mPb.NSReq) (*mPb.NSResp, error
 	return rsp, nil
 }
 
-func (s *Server) Delete(ctx context.Context, req *mPb.NSReq) (error, *mPb.NSResp) {
+func (s *Server) Delete(ctx context.Context, req *mPb.NSReq) (*mPb.NSResp, error) {
 	span, _ := sg.FromGRPCContext(ctx, "meridian.Delete")
 	defer span.Finish()
 	fmt.Println(span)
 
 	token, err := helper.ExtractToken(ctx)
 	if err != nil {
-		span.AddLog(&sg.KV{"token error", err.Error()})
-		return err, nil
+		span.AddLog(&sg.KV{Key: "token error", Value: err.Error()})
+		return nil, err
 	}
 
+	// check this later
 	// _, err = s.checkNS(ctx, req.Mutate.UserId, req.Mutate.Namespace)
 	// if err != nil {
-	// 	span.AddLog(&sg.KV{"check ns error", err.Error()})
-	// 	return err, nil
+	// 	span.AddLog(&sg.KV{Key: "check ns error", Value: err.Error()})
+	// 	return nil, err
 	// }
 
 	err, rsp := s.db.Delete(
@@ -136,11 +142,13 @@ func (s *Server) Delete(ctx context.Context, req *mPb.NSReq) (error, *mPb.NSResp
 			token,
 		), req,
 	)
+
 	if err != nil {
-		span.AddLog(&sg.KV{"namespace exists error", err.Error()})
-		return err, nil
+		span.AddLog(&sg.KV{Key: "namespace exists error", Value: err.Error()})
+		return nil, err
 	}
-	return nil, rsp
+
+	return rsp, nil
 }
 
 func Run(db storage.DB, conf *model.Config) {
