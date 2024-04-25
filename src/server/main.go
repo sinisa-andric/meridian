e2139c5b9c101918f940da6cf9ad3bbcd6d664f4
sinisa-andric <sinisa.andric@mail.ru>
2024-04-25 12:06:41 +0200

    feat: crud functions place update

--- src/server/main.go
+++ src/server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net"
 
@@ -10,6 +11,38 @@ import (
 
 type server struct {
 	namespace.UnimplementedNameSpaceServiceServer
+}
+
+func (s *server) SayHi(ctx context.Context, in *namespace.NameSpaceRequest) (*namespace.NameSpaceResponse, error) {
+	return &namespace.NameSpaceResponse{Data: "Hi!"}, nil
+}
+
+func (s *server) Check(ctx context.Context, in *namespace.NameSpaceRequest) (*namespace.NameSpaceResponse, error) {
+	return &namespace.NameSpaceResponse{Data: "Check!"}, nil
+}
+
+func (s *server) Create(ctx context.Context, in *namespace.NameSpaceRequest) (*namespace.NameSpaceResponse, error) {
+	return &namespace.NameSpaceResponse{Data: "Create!"}, nil
+}
+
+func (s *server) Read(ctx context.Context, in *namespace.NameSpaceRequest) (*namespace.NameSpaceResponse, error) {
+	return &namespace.NameSpaceResponse{Data: "Read!"}, nil
+}
+
+func (s *server) Update(ctx context.Context, in *namespace.NameSpaceRequest) (*namespace.NameSpaceResponse, error) {
+	return &namespace.NameSpaceResponse{Data: "Update!"}, nil
+}
+
+func (s *server) Delete(ctx context.Context, in *namespace.NameSpaceRequest) (*namespace.NameSpaceResponse, error) {
+	return &namespace.NameSpaceResponse{Data: "Delete!"}, nil
+}
+
+func (s *server) SoftDelete(ctx context.Context, in *namespace.NameSpaceRequest) (*namespace.NameSpaceResponse, error) {
+	return &namespace.NameSpaceResponse{Data: "Soft Delete!"}, nil
+}
+
+func (s *server) Describe(ctx context.Context, in *namespace.NameSpaceRequest) (*namespace.NameSpaceResponse, error) {
+	return &namespace.NameSpaceResponse{Data: "Describe!"}, nil
 }
 
 func main() {
